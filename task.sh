b564af6ed516725c66d7fe63f0d965c9783643f3
elior <elior.estrin@develeap.com>
2023-11-06 15:42:26 +0200

    sshd_config

--- task.sh
+++ task.sh
@@ -11,5 +11,25 @@ usermod -aG wheel,ctera ctera
 
 
 # edit sshd_config
+edit_sshd_config() {
+  file="/etc/ssh/sshd_config"  # Define the file to edit.
+  for PARAM in "${param[@]}"
+  do
+    /usr/bin/sed -i "/^${PARAM}/d" "$file"
+    echo "All lines beginning with '${PARAM}' were deleted from $file."
+  done
+  
+  for PARAM_VALUE in "${param_values[@]}"
+  do
+    echo "$PARAM_VALUE" >> "$file"
+    echo "'$PARAM_VALUE' was added to $file."
+  done
+}
+
+# Usage of the function should define both the parameters to be deleted and the full lines to be added
+param=("PasswordAuthentication" "PubkeyAuthentication" "AuthorizedKeysFile")
+param_values=("PasswordAuthentication no" "PubkeyAuthentication yes" "AuthorizedKeysFile .ssh/authorized_keys")
+edit_sshd_config
+
 
 
